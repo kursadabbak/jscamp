4d8fbc6e7693253d18c063120bb3c7635e3417e0
kursadabbak <afsinkursad@hotmail.com>
2021-10-19 00:24:04 +0300

    ders sonu

---
+++ src/app.js
@@ -0,0 +1,17 @@
+console.log("Merhaba Kodlama.io");
+
+let dolarBugun = 9.30
+let dolarDun= 9.20
+
+const euroDun= 11.20
+//euroDun=11     const sabittir değerini değiştiremeyiz
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi" , "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++){
+    console.log("<ul>"+konutKredileri[i]+"</ul>")
+}
+console.log("</ul>")
